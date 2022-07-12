47f0303e9c7eaba0751259237aca1ce09e3b9e00
Dayu Bian <dayub@chinasystems.com>
2022-07-12 15:05:09 +0800

    init

---
+++ src/main/java/com/example/demo/RestDslRouteBuilder.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+/**
+ * This RouteBuilder defines our REST API using Camel's REST DSL.
+ *
+ * A RestConfiguration block first defines how the service will be instantiated.
+ * The rest() DSL block then defines each of the RESTful service operations.
+ */
+@Component
+public class RestDslRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        // This section is required - it tells Camel how to configure the REST service
+        restConfiguration()
+                // Use the 'servlet' component.
+                // This tells Camel to create and use a Servlet to 'host' the RESTful API.
+                // Since we're using Spring Boot, the default servlet container is Tomcat.
+                .component("servlet")
+
+                // Allow Camel to try to marshal/unmarshal between Java objects and JSON
+                .bindingMode(RestBindingMode.auto);
+
+        rest("/say")
+            .get("/hello").to("direct:hello")
+            .get("/bye").consumes("application/json").to("direct:bye")
+            .post("/bye").to("mock:update");
+    }
+}
